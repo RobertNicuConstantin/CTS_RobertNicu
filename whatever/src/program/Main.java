e229509b8aa8e8f1ad569b767eb2ca5ec9e58cf3
ventu <ventu@192.168.0.227>
2021-02-25 17:18:24 +0200

    Adugarea claselor pt gradina zoologica

--- whatever/src/program/Main.java
+++ whatever/src/program/Main.java
@@ -1,10 +1,18 @@
 package program;
 
+import clase.Giraffe;
+import clase.Zoo;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Zoo z = new Zoo();
+		Giraffe giraffe = new Giraffe("girafa 1");
+		Giraffe giraffe2 = new Giraffe("girafa 2");
+		z.AddAnimal(giraffe);
+		z.AddAnimal(giraffe2);
+		
+		z.feedAllAnimals();
 	}
 
 }
