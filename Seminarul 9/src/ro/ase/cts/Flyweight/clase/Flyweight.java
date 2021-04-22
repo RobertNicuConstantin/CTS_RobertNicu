ce70626912efd7d16d14f2afb2c74e74fc661857
ventu <ventu@192.168.0.227>
2021-04-22 16:17:38 +0300

    Flyweight

---
+++ Seminarul 9/src/ro/ase/cts/Flyweight/clase/Flyweight.java
@@ -0,0 +1,7 @@
+package ro.ase.cts.Flyweight.clase;
+
+public interface Flyweight {
+	
+	public void printeazaRezervare(Rezervare r);
+
+}
---
+++ Seminarul 9/src/ro/ase/cts/Flyweight/clase/FlyweightFactory.java
@@ -0,0 +1,27 @@
+package ro.ase.cts.Flyweight.clase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FlyweightFactory {
+	private Map<String, Flyweight> clienti;
+	
+	
+	
+	public FlyweightFactory() {
+		super();
+		this.clienti = new HashMap<String,Flyweight>();
+	}
+
+
+
+	public Flyweight getClient(String nrTelefon) {
+		Flyweight flyweight = clienti.get(nrTelefon);
+		if (flyweight==null) {
+			flyweight = new Client("Ion", nrTelefon, "asd@gmail.com");
+			clienti.put(nrTelefon, flyweight);
+		}
+		return flyweight;
+	}
+ 
+}
