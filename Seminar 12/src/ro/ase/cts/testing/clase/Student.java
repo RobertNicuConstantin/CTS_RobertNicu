339ef746e10bd62688318a422667acd5f63ce491
ventu <ventu@192.168.0.227>
2021-05-20 19:41:22 +0300

    JUnit Testing

--- Seminar 12/src/ro/ase/cts/testing/clase/Student.java
+++ Seminar 12/src/ro/ase/cts/testing/clase/Student.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Student {
-    private String nume;
+	private String nume;
     private List<Integer> note;
 
     public Student(String nume) {
--- Seminar 12/src/ro/ase/cts/testing/tests/StudentTests.java
+++ Seminar 12/src/ro/ase/cts/testing/tests/StudentTests.java
@@ -6,36 +6,36 @@ import org.junit.Test;
 
 import ro.ase.cts.testing.clase.Student;
 
+
+
 public class StudentTests {
 
 	@Test
 	public void testConstructorCuParametru() {
-		String nume = "Costica";
+		String nume = "Alice"; 
 		Student student = new Student(nume);
 		
 		assertEquals(nume, student.getNume());
-		assertEquals(nume, student.getNume());
 		assertNotNull(student.getNote());
 		
 	}
-	
 	
 	@Test
-	public void testConstructorFaraParametrii() {
+	public void testConstructorFaraParametru() {
 		Student student = new Student();
 		
-		assertNotNull(student.getNote());
-		assertNotNull(student.getNume());
+		assertNotNull("Atributul note nu a fost initializat", student.getNote());
+		assertNotNull("Atributul nume nu a fost initializat", student.getNume());
 	}
-	
 	
 	@Test
 	public void testAdaugaNota() {
 		Student student = new Student();
 		
-		int nota = 5;
-		student.adaugaNota(5);
-		assertEquals(nota,student.getNota(0));
+		int nota = 10;
+		student.adaugaNota(nota);
+		assertEquals(nota, student.getNota(0));
+		
 	}
 	
 	@Test
@@ -43,48 +43,95 @@ public class StudentTests {
 		Student student = new Student();
 		
 		student.adaugaNota(5);
+		student.adaugaNota(7);
+		assertEquals(2, student.getNote().size());
+	}
+	
+	@Test
+	public void testCalculeazaMedie() {
+		Student student = new Student();
+		
+		student.adaugaNota(9);
 		
 		student.adaugaNota(8);
 		
-		assertEquals(2,student.getNote().size());
-		
+		assertEquals(8.5f, student.calculeazaMedie(), 0.1);
 	}
 	
 	@Test
-	public void testCaluleazaMedieNote() {
-		
+	public void testCalculeazaMedieFaraNote() {
 		Student student = new Student();
 		
-		student.adaugaNota(5);
-		
-		student.adaugaNota(8);
-		
-		assertEquals(6.5f, student.calculeazaMedie(),0.1);
+		assertEquals(0, student.calculeazaMedie(), 0.1);
 	}
 	
 	@Test
-	public void testCalculeazaMediefaraNote() {
+	public void testCalculeazaMedieCuONota() {
 		Student student = new Student();
-		student.adaugaNota(5);
 		
+		student.adaugaNota(6);
 		
-		assertEquals(5,student.calculeazaMedie(),0.1);
+		assertEquals(6, student.calculeazaMedie(), 0.1);
 	}
 	
 	@Test
-	public void testAreRestanteAdevarat() {
+	public void testAreRestante() {
 		Student student = new Student();
+		
 		student.adaugaNota(9);
 		student.adaugaNota(3);
+		
 		assertTrue(student.areRestante());
 	}
 	
-	
-	public void testAreRestanteFals() {
+	@Test
+	public void testNuAreRestante() {
 		Student student = new Student();
+		
 		student.adaugaNota(9);
-		student.adaugaNota(3);
-		assertTrue(student.areRestante());
+		student.adaugaNota(7);
+		
+		assertFalse(student.areRestante());
 	}
 	
+	//jUnit 3
+	@Test
+	public void testGetNotaExceptie() {
+		Student student = new Student();
+		
+		student.adaugaNota(4);
+		student.adaugaNota(9);
+		
+		try {
+			student.getNota(5);
+			fail("Metoda nu arunca exceptie");
+		} catch (IndexOutOfBoundsException e) {
+			
+		} catch (Exception e) {
+			fail("Metoda arunca o alta exceptie");
+		}
+	}
+	
+	//jUnit4
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void testGetNotaExceptieV4() {
+		Student student = new Student();
+		
+		student.adaugaNota(4);
+		student.adaugaNota(9);
+		
+		student.getNota(5);
+	}
+	
+	//jUnit 5 
+//	@Test
+//	public void testGetNotaExceptieV5() {
+//		Student student = new Student();
+//		
+//		student.adaugaNota(4);
+//		student.adaugaNota(9);
+//		
+//		student.getNota(5);
+//		assertThrows(IndexOutOfBoundsException.class, () -> student.getNota(5));
+//	}
 }
